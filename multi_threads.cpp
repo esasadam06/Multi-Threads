33f6769337c958176e003115286a65287bf529aa
esasadam06 <48632551+esasadam06@users.noreply.github.com>
2021-04-13 21:23:52 +0300

    update multi thread

--- multi_threads.cpp
+++ multi_threads.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<pthread.h>
 #include<ctime>
+#include<iomanip>
 
 using namespace std;
 
@@ -20,46 +21,57 @@ void *asalArama(void *args){
 	int *limit = (int*) args;
 	for(int i = limit[0];i <=limit[1];i++){
 		if(asalSayi(i)){
-			//cout << " " << i << " ";
+			//cout << " " << limit[2] << " ";
 		}
 	}
 }
 
 int main(){
 	
-	int kanal_sayisi, rnd, aralik, starts = 0;
+	int kanal_sayisi, rnd = 200000, aralik, starts = 0;
 	time_t start, finish;
-	cout << "Sistem Testi icin asal bulma islemi yapilacaktir. \nHangi sayiya kadar islem devam etsin: ";
-	cin >> rnd;
-	do{
-		cout << " " << rnd  <<" tam bolen bir say¿ giriniz!..";
-		cin >> kanal_sayisi;
-		aralik = rnd/kanal_sayisi;	
-	}while(rnd%kanal_sayisi!=0);
+	int dif[5];
+	int cekirdek[] = {1,2,4,8,16,32};
+
+	for(int j=0;j<6;j++){
+		starts = 0;
+		kanal_sayisi = cekirdek[j];
+		cout << "\n" << kanal_sayisi;
+		aralik = rnd/kanal_sayisi;
+		int arry[kanal_sayisi][3];
+		pthread_t kanal[kanal_sayisi];
 	
-	int arry[kanal_sayisi][2];
-	pthread_t kanal[kanal_sayisi];
+		for(int i=0;i<kanal_sayisi;i++){
+			arry[i][0]=starts;
+			starts+=aralik;
+			arry[i][1]=starts;
+			arry[i][2]=i;
+			//cout << "start :" << arry[i][0] << " end :" << arry[i][1] << " ";
+		}
+		start = clock();
+		for(int i=0; i<kanal_sayisi; i++){
+			pthread_create(&kanal[i],NULL,asalArama,arry[i]);
+		}
+		for(int i=0; i<kanal_sayisi; i++){
+			pthread_join(kanal[i],NULL);
+		}
 	
-	for(int i=0;i<kanal_sayisi;i++){
-		arry[i][0]=starts;
-		starts+=aralik;
-		arry[i][1]=starts;
-		//cout << "start :" << arry[i][0] << " end :" << arry[i][1] << " ";
+		finish = clock();
+		dif[j] = difftime(finish, start);
+		cout << "\tThread ile harcanan zaman = " << dif[j] << " milisaniye";
 	}
-	start = clock();
-	for(int i=0; i<kanal_sayisi; i++){
-		pthread_create(&kanal[i],NULL,asalArama,arry[i]);
-	}
-	for(int i=0; i<kanal_sayisi; i++){
-		pthread_join(kanal[0],NULL);
-	}
 	
-	aramaAralik(0,40000);
-	finish = clock();
+	cout << "\n";
 	
-	cout << "\n Harcanan zaman = " << difftime(finish, start) << " milisaniye";
+	for(int i=0; i<6; i++){
+		float temp = dif[i];
+		float ilk = dif[0];
+		float spd = ilk/temp;
+		float verim = spd/cekirdek[i];
+		cout << fixed << setprecision(5);
+		cout << cekirdek[i] << "\tThread kullanimi ile hiz: " << spd << "\tVerim: " << verim <<endl;		
+	}
+
 	getchar();
-	system("PAUSE");
 	return 0;
 }
-
